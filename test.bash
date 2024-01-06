af1b25b3b8b8c54896555148d3f604bceb8ec97d
hibiki703 <hibiki703@LAPTOP-5J6C32VG.localdomain>
2024-01-07 02:44:48 +0900

    test

--- test.bash
+++ test.bash
@@ -13,16 +13,16 @@ res=0
 out=$(seq 2 | ./plus)
 [ "${out}" = "1項目: 1, 現在の総計: 1
 2項目: 2, 現在の総計: 3
-最終総計: 3"] || ng {$LINENO}
+最終総計: 3"] || ng $LINENO
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]     || ng {$LINENO}
-[ "${out}" = "" ] || ng {$LINENO}
+[ "$res" = 1 ]     || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
 
 out=$(echo | ./plus)
-[ "$?" = 1 ]     || ng {$LINENO}
-[ "${out}" = "" ] || ng {$LINENO}
+[ "$res" = 1 ]     || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
 
 [ "$res" = 0 ] && echo OK
 exit $res
